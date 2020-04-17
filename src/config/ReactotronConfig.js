dcdce230bce40d56741df2e462f8396e70134d9e
Diego Ferreira <diferreira.program@gmail.com>
2020-04-17 15:53:57 -0300

    Finalizado parte de redux e saga

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,8 +1,12 @@
 import Reactotron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
+import reacttotronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
-   const tron = Reactotron.configure().use(reactotronRedux()).connect();
+   const tron = Reactotron.configure()
+      .use(reactotronRedux())
+      .use(reacttotronSaga())
+      .connect();
 
    tron.clear();
 
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -6,13 +6,13 @@ import { formatPrice } from '../../util/format';
 import { MdExpandMore, MdExpandLess, MdDelete } from 'react-icons/md';
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, total, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
    function increment(product) {
-      updateAmount(product.id, product.amount + 1);
+      updateAmountRequest(product.id, product.amount + 1);
    }
 
    function decrement(product) {
-      updateAmount(product.id, product.amount - 1);
+      updateAmountRequest(product.id, product.amount - 1);
    }
 
    return (
